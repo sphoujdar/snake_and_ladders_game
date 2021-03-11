35a2b38bc034b589717944a6807345d2595ceaa9
Shubham <sm.phoujdar@gmail.com>
2021-03-11 13:38:55 +0530

    Added : Constants required for further use cases.

--- src/main/java/snake_and_ladders/GameRunner.java
+++ src/main/java/snake_and_ladders/GameRunner.java
@@ -1,9 +1,18 @@
 package snake_and_ladders;
-
+	
 public class GameRunner {
+		
+		//Constants
+		public static final int WINNING_POSITION = 100;
+		public static final int SNAKE_CONSTANT = 0;
+		public static final int NO_PLAY_CONSTANT = 1;
+		public static final int LADDER_CONSTANT = 2;
+		
+		
 		public static void main(String[] args) {
 			
 			int player_position = 0;
 			System.out.printf("Snake and ladders game :\nYou are at position %d.", player_position);
+			
 		}
 }
