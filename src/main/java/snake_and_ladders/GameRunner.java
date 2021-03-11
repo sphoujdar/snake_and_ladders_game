bd6124929e74f39c0c02e8c83351feaf7d607bf5
Shubham <sm.phoujdar@gmail.com>
2021-03-11 13:33:06 +0530

    Added : First Commit to master , Player at position 0.

---
+++ src/main/java/snake_and_ladders/GameRunner.java
@@ -0,0 +1,9 @@
+package snake_and_ladders;
+
+public class GameRunner {
+		public static void main(String[] args) {
+			
+			int player_position = 0;
+			System.out.printf("Snake and ladders game :\nYou are at position %d.", player_position);
+		}
+}
